11295c203f58cadaea9313b9d5e614d16df11e72
root <root@DESKTOP-U07HU7L>
2025-04-08 19:44:20 +0500

    loops in pyhton

---
+++ loops.py
@@ -0,0 +1,19 @@
+list_of_cloud = ["AWS" , "Azure" , "GCP" , "digital Ocean"]
+print (list_of_cloud)
+
+list_of_cloud.append("Oracle")
+print (list_of_cloud)
+
+list_of_cloud.insert(2, "Salseforce")
+print (list_of_cloud)
+
+print(len(list_of_cloud))
+
+for cloud in list_of_cloud:
+    print (cloud)
+
+for i in range(1,11):
+    print (i)
+
+for i in range(1,11):
+    print (cloud)
